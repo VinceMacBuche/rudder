aab80266318eddda68c190412455af9fe96c01f3
Vincent Membré <vincent.membre@rudder.io>
2023-12-05 12:20:32 +0100

    Start repository²

--- webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/score/Score.scala
+++ webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/score/Score.scala
@@ -3,39 +3,38 @@ package com.normation.rudder.score
 import com.normation.errors.IOResult
 import com.normation.inventory.domain.NodeId
 import com.normation.rudder.domain.reports.CompliancePercent
-import com.normation.rudder.score.ScoreValue.A
-import com.normation.rudder.score.ScoreValue.B
-import com.normation.rudder.score.ScoreValue.C
-import com.normation.rudder.score.ScoreValue.D
+import com.normation.rudder.score.ScoreValue.{A, B, C, D, E}
 import com.normation.zio._
 import zio.Ref
 import zio.ZIO
 import zio.syntax._
 
-sealed trait ScoreValue
+sealed trait ScoreValue {
+  def value : String
+}
 
 object ScoreValue {
-  case object A extends ScoreValue
-  case object B extends ScoreValue
-  case object C extends ScoreValue
-  case object D extends ScoreValue
-  case object E extends ScoreValue
+  case object A extends ScoreValue { val value = "A" }
+  case object B extends ScoreValue { val value = "B" }
+  case object C extends ScoreValue { val value = "C" }
+  case object D extends ScoreValue { val value = "D" }
+  case object E extends ScoreValue { val value = "E" }
+
+  val allValues: Set[ScoreValue] = ca.mrvisser.sealerate.values
+
+  def fromString(s: String) = allValues.find(_.value == s.toLowerCase()) match {
+    case None => Left(s"${s} is not valid status value, accepted values are ${allValues.map(_.value).mkString(", ")}")
+    case Some(v) => Right(v)
+  }
 }
 
 trait ScoreDetails
 
 case class ComplianceScoreDetails(compliance: CompliancePercent) extends ScoreDetails
 
-trait Score {
-  def name:    String
-  def value:   ScoreValue
-  def message: String
-  def details: ScoreDetails
-}
+case class Score (name:    String,value:   ScoreValue, message: String, details: ScoreDetails)
 
-case class ComplianceScore(value: ScoreValue, message: String, details: ComplianceScoreDetails) extends Score {
-  override def name: String = "compliance"
-}
+case class ComplianceScore(override val value: ScoreValue,override val  message: String,override val  details: ComplianceScoreDetails) extends Score("compliance", value, message, details)
 
 case class GlobalScore(value: ScoreValue, message: String, details: List[Score])
 
@@ -79,17 +78,46 @@ object ComplianceScoreEventHandler extends ScoreEventHandler {
         } else if (p.compliance >= 25) {
           ComplianceScore(D, "Node is compliant at least at 25%", ComplianceScoreDetails(p))
         } else {
-          ComplianceScore(D, "Node is compliant at less then 25%", ComplianceScoreDetails(p))
+          ComplianceScore(E, "Node is compliant at less then 25%", ComplianceScoreDetails(p))
         }
-
         ((n, score :: Nil) :: Nil).succeed
       case _                          => Nil.succeed
     }
   }
 }
 
+
 class ScoreService {
   val cache: Ref[Map[NodeId, GlobalScore]] = Ref.make(Map[NodeId, GlobalScore]()).runNow
+
+  def getAll() : IOResult[Map[NodeId, GlobalScore]] = cache.get
+
+  def cleanScore(name: String) = {
+    for {
+      _ <- cache.update(_.map { case (id, gscore) => (id, gscore.copy(details = gscore.details.filterNot(_.name == name))) })
+    } yield {}
+  }
+
+  def update(newScores : Map[NodeId,List[Score]]) = {
+    for {
+      c <- cache.get
+      updatedValue = (for {
+        (nodeId, newScores) <- newScores
+      } yield {
+        val oldScores = c.get(nodeId) match {
+          case None => Nil
+          case Some(oldScore) => oldScore.details
+        }
+        (nodeId, GlobalScoreService.computeGlobalScore(oldScores, newScores))
+      })
+      updatedCache <- ZIO.foreach(updatedValue.toList) { case (nodeId, score) => cache.update(_.+((nodeId, score))) }
+    } yield {}
+
+  }
+}
+
+
+class ScoreServiceManager(readScore : ScoreService) {
 
   val handlers: Ref[List[ScoreEventHandler]] = Ref.make(ComplianceScoreEventHandler :: List.empty[ScoreEventHandler]).runNow
 
@@ -97,28 +125,11 @@ class ScoreService {
     handlers.update(handler :: _)
   }
 
-  def cleanScore(name: String)            = {
-    for {
-      _ <- cache.update(_.map { case (id, gscore) => (id, gscore.copy(details = gscore.details.filterNot(_.name == name))) })
-    } yield {}
-  }
   def handleEvent(scoreEvent: ScoreEvent) = {
     for {
       h            <- handlers.get
       newScore     <- ZIO.foreach(h)(_.handle(scoreEvent)).map(_.flatten.groupMapReduce(_._1)(_._2)(_ ++ _))
-      c            <- cache.get
-      updatedValue  = (for {
-                        (nodeId, newScores) <- newScore
-                      } yield {
-                        val oldScores = c.get(nodeId) match {
-                          case None           => Nil
-                          case Some(oldScore) => oldScore.details
-                        }
-                        (nodeId, GlobalScoreService.computeGlobalScore(oldScores, newScores))
-                      })
-      updatedCache <- ZIO.foreach(updatedValue.toList) { case (nodeId, score) => cache.update(_.+((nodeId, score))) }
-
+      _ <- readScore.update(newScore)
     } yield {}
   }
-
 }
