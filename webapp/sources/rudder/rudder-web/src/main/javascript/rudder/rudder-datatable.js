b17c83c7d589c9dadbca8116a69e438681d1b684
Clark Andrianasolo <clark.andrianasolo@rudder.io>
2024-03-19 12:04:45 +0100

    fixup! fixup! Fixes #24528: Nodes tables height is too small
    Fixes #24528: Nodes tables height is too small

--- webapp/sources/rudder/rudder-web/src/main/javascript/rudder/rudder-datatable.js
+++ webapp/sources/rudder/rudder-web/src/main/javascript/rudder/rudder-datatable.js
@@ -1366,7 +1366,7 @@ function createNodeTable(gridId, refresh) {
       bottom = $('.main-details > .table-container'),
       handle = $('#drag');
 
-    if (handle) {
+    if (handle.length) {
       hasHandle = true;
     }
     handle.on('mousedown', function (e) {
